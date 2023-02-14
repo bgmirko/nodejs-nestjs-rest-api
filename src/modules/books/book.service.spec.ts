9561cf90cf9f091c370609a5b65b178e3d7853ea
Mirko Martinovic <mmartinovic@universe.dart.spb@mmartinovic.nb.ipa.dataart.net>
2023-02-14 15:50:45 +0100

    test book service with real database, inject database to service

--- src/modules/books/book.service.spec.ts
+++ src/modules/books/book.service.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookService } from './book.service';
+import { DatabaseModule } from '../../database/database.module';
+import { bookProviders } from './book.providers';
 
 describe('BooksService', () => {
   let service: BookService;
+  const mockBookRepository = {
+
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BookService],
+      imports: [DatabaseModule],
+      providers: [BookService, ...bookProviders],
     }).compile();
 
     service = module.get<BookService>(BookService);
@@ -15,4 +21,9 @@ describe('BooksService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('Expect to get data from database', async () => {
+    const result = await service.getBookById(1);
+    expect(typeof result).toBe('object');
+  })
 });
