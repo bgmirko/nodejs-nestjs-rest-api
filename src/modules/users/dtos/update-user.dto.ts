dcf547df824ec58c351343c15b4caedfa16a80e2
bgmirko <bgmirko@hotmail.com>
2023-05-17 15:41:22 +0200

    fix type

--- src/modules/users/dtos/update-user.dto.ts
+++ src/modules/users/dtos/update-user.dto.ts
@@ -1,6 +1,8 @@
-import { OmitType } from '@nestjs/swagger';
+import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends OmitType(CreateUserDto, [
+class UpdateUserOmitDto extends OmitType(CreateUserDto, [
   'password',
 ] as const) {}
+
+export class UpdateUserDto extends PartialType(UpdateUserOmitDto) {}
